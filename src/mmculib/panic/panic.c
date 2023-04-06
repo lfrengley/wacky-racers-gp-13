5ff218acbde1fb26f72a344b2138c2dfa98d09c8
Harry Mander <harrymander96@gmail.com>
2023-04-06 15:05:18 +1200

    mmculib: panic: fix undeclared symbol error

--- src/mmculib/panic/panic.c
+++ src/mmculib/panic/panic.c
@@ -5,12 +5,7 @@ void
 panic (pio_t led_error_pio, unsigned int error_code)
 {
     unsigned int i;
-
     pio_config_set (led_error_pio, PIO_OUTPUT_LOW);
-#ifndef LED_ACTIVE
-    pio_output_set (led_error_pio, LED_ACTIVE);
-#endif
-
     while (1)
     {
         for (i = 0; i < error_code; i++)
