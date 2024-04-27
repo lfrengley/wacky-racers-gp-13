5d523b2cb897f4b845a2cc3af99ebc890afaf061
Hayden Evans <hev23@uclive.ac.nz>
2024-04-27 13:56:01 +1200

    added moving average filter, and accel structs. Untested...

--- src/apps/hat/accelerometer.c
+++ src/apps/hat/accelerometer.c
@@ -17,6 +17,20 @@ static uint16_t TO_DEG = 57296; // 180 * 1000 / pi
 static twi_t adxl345_twi;
 static adxl345_t *adxl345;
 
+typedef struct {
+    int16_t x;
+    int16_t y;
+    int16_t z;
+} AccelVector;
+
+static uint8_t NUM_SAMPLES = 3;
+typedef struct {
+    AccelVector samples[NUM_SAMPLES];
+    uint8_t index;
+} MovingAverageFilter;
+
+static MovingAverageFilter filter;
+
 static twi_cfg_t adxl345_twi_cfg =
 {
     .channel = TWI_CHANNEL_0,
@@ -24,21 +38,44 @@ static twi_cfg_t adxl345_twi_cfg =
     .slave_addr = 0
 };
 
-static void calculate_percent_gravity(int16_t raw_accel[3], int32_t *ax, int32_t *ay, int32_t *az) {
+static void init_moving_average_filter(MovingAverageFilter *filter) {
+    for (uint8_t i = 0; i < NUM_SAMPLES; i++) {
+        filter->samples[i].x = 0;
+        filter->samples[i].y = 0;
+        filter->samples[i].z = 0;
+    }
+    filter->index = 0;
+}
+
+static void write_accel_vector(AccelVector *accel, int16_t accel_array[3]) {
+    accel->x = accel_array[0];
+    accel->y = accel_array[1];
+    accel->z = accel_array[2];
+}
+
+static void calculate_percent_gravity(AccelVector *avg_raw_accel, AccelVector *accel_gravity_perc) {
+    /* 
+    Takes raw accelerometer data, and converts it to percent gravity using
+    the range of the accelerometer output (+-Range ()) and the resulotion 
+    of the accelerometer output in bits (N)
+    */
+
     int16_t resolution = pow(2, N-1);
 
     // acceleration as a percentage of gravity 
-    // (raw_accel * 100 /2^(n-1)) * abs(range)
-    *ax = (raw_accel[0] * 100 /resolution) * RANGE;
-    *ay = (raw_accel[1] * 100 /resolution) * RANGE;
-    *az = (raw_accel[2] * 100 /resolution) * RANGE;
+    accel_gravity_perc->x = (avg_raw_accel->x * 100 /resolution) * RANGE;
+    accel_gravity_perc->y = (avg_raw_accel->y * 100 /resolution) * RANGE;
+    accel_gravity_perc->z = (avg_raw_accel->z * 100 /resolution) * RANGE;
 }
 
-static void calculate_pitch_roll(int32_t ax, int32_t ay, int32_t az, int32_t *pitch, int32_t *roll) {
+static void calculate_pitch_roll(AccelVector *accel_gravity_perc, int32_t *pitch, int32_t *roll) {
     // Calculate pitch angle
-    *pitch = (int32_t)((asinf(ax / sqrtf((float)(ax * ax + ay * ay + az * az))) * TO_DEG));
+    int16_t x = accel_gravity_perc->x;
+    int16_t y = accel_gravity_perc->y;
+    int16_t z = accel_gravity_perc->z;
+    *pitch = (int32_t)((asinf(x / sqrtf((float)(x*x + y*y + z*z))) * TO_DEG));
     // Calculate roll angle
-    *roll = (int32_t)((atan2f(ay, az) * TO_DEG));
+    *roll = (int32_t)((atan2f(y, z) * TO_DEG));
 }
 
 void init_accelerometer(void) {
@@ -51,6 +88,33 @@ void init_accelerometer(void) {
     adxl345 = adxl345_init (adxl345_twi, ADXL345_ADDRESS);   
         if (! adxl345)
         panic (LED_ERROR_PIO, 2);
+
+    init_moving_average_filter(&filter);
+}
+
+static void update_moving_average_filter(MovingAverageFilter *filter, AccelVector *raw_accel) {
+    filter->samples[filter->index].x = raw_accel->x;
+    filter->samples[filter->index].y = raw_accel->y;
+    filter->samples[filter->index].z = raw_accel->z;
+
+    filter->index++;
+    if (filter->index >= NUM_SAMPLES) {
+        filter->index = 0;
+    }
+}
+
+static void calc_moving_average(const MovingAverageFilter *filter, AccelVector *avg_accel) {
+    int32_t sum_x = 0, sum_y = 0, sum_z = 0;
+
+    for (uint8_t i = 0; i < NUM_SAMPLES; i++) {
+        sum_x += filter->samples[i].x;
+        sum_y += filter->samples[i].y;
+        sum_z += filter->samples[i].z;
+    }
+
+    avg_accel->x = sum_x / NUM_SAMPLES;
+    avg_accel->y = sum_y / NUM_SAMPLES;
+    avg_accel->z = sum_z / NUM_SAMPLES;
 }
 
 bool check_accelerometer(void) {
@@ -60,16 +124,23 @@ bool check_accelerometer(void) {
             printf ("Waiting for accelerometer to be ready...\n");
             accelerometer_state = false;
         } else {
-            int16_t accel[3];
-            if (adxl345_accel_read (adxl345, accel)) {
+            int16_t accel_array[3];
+            AccelVector raw_accel;
+            AccelVector avg_raw_accel;
+            AccelVector accel_gravity_perc;
+
+            if (adxl345_accel_read (adxl345, accel_array)) {
+                write_accel_vector(&raw_accel, accel_array);
+                update_moving_average_filter(&filter, &raw_accel);
+                calc_moving_average(&filter, &avg_raw_accel);
+
                 // Variables to store pitch and roll angles
                 int32_t pitch, roll;
-                int32_t ax, ay, az;
-                
-                calculate_percent_gravity(accel, &ax, &ay, &az);
-                calculate_pitch_roll(ax, ay, az, &pitch, &roll);
 
-                printf ("x: %5ld  y: %5ld  z: %5ld\n", ax, ay, az); 
+                calculate_percent_gravity(&avg_raw_accel, &accel_gravity_perc);
+                calculate_pitch_roll(&accel_gravity_perc, &pitch, &roll);
+
+                printf ("x: %5ld  y: %5ld  z: %5ld\n", accel_gravity_perc.x, accel_gravity_perc.y, accel_gravity_perc.z); 
                 printf ("pitch: %3ld deg\troll %3ld deg\n", pitch/1000, roll/1000); //pitch doesn't go full 180?
                 
                 accelerometer_state = true;
