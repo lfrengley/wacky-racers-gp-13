9747e37e5ac96cb9acb70b47c1e4ff62df82f601
Hayden Evans <hev23@uclive.ac.nz>
2024-05-08 13:25:54 +1200

    added scheduler to car

--- src/apps/racer/racer.c
+++ src/apps/racer/racer.c
@@ -12,6 +12,31 @@
 #include "pio.h"
 #include "usb_serial.h"
 #include "motors.h"
+#include "scheduler.h"
+
+#define PACER_RATE 20
+#define SERIAL_POLL_RATE 1
+#define STATUS_LED_BLINK_RATE 100
+
+void toggle_status_led(void) {
+    pio_output_toggle (LED_STATUS_PIO);
+    // printf("Toggling Status LED\n");
+}
+
+void scan_serial(void) {
+    int16_t pwm_left, pwm_right;
+    char buf[256];
+    if (fgets(buf, sizeof(buf), stdin)) {
+        // sscanf returns the number of input items successfully matched
+        if (sscanf(buf, "%hd %hd",&pwm_left, &pwm_right) == 2) {
+            set_motor_duties(pwm_left, pwm_right);
+            printf("%hd\n",  pwm_left);
+            printf("%hd\n", pwm_right);
+        } else {
+            printf("Invalid input\n");
+        }
+    }
+}
 
 /* Initialise */
 void init(void) {
@@ -23,7 +48,11 @@ void init(void) {
     pio_output_set (LED_ERROR_PIO, ! LED_ACTIVE);
     pio_config_set (LED_STATUS_PIO, PIO_OUTPUT_LOW);
     pio_output_set (LED_STATUS_PIO, ! LED_ACTIVE);
-    
+
+    sysclock_init();
+    add_task(&toggle_status_led, STATUS_LED_BLINK_RATE);
+    add_task(&scan_serial, SERIAL_POLL_RATE);
+
     init_motors();
 }
 
@@ -31,21 +60,7 @@ void init(void) {
 int
 main (void)
 {
-    int16_t pwm_left, pwm_right;
+    
     init();
-
-    while (1) {
-
-        char buf[256];
-        if (fgets(buf, sizeof(buf), stdin)) {
-            // sscanf returns the number of input items successfully matched
-            if (sscanf(buf, "%hd %hd",&pwm_left, &pwm_right) == 2) {
-                set_motor_duties(pwm_left, pwm_right);
-                printf("%hd\n",  pwm_left);
-                printf("%hd\n", pwm_right);
-            } else {
-                printf("Invalid input\n");
-            }
-        }
-    }
+    run_scheduler();
 }
