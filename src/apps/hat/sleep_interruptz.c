9e0da14b343fb47b253e14d572e27704071aa5cc
lfrengley <lfrengley@gmail.com>
2024-05-15 16:13:31 +1200

    penny interrupt sleep button

---
+++ src/apps/hat/sleep_interruptz.c
@@ -0,0 +1,73 @@
+#include "target.h"
+#include "mcu_sleep.h"
+#include "config.h"
+#include "mcu.h"
+#include "cpu.h"
+#include "irq.h"
+
+//Button includes:
+#include "pio.h"
+#include "pacer.h"
+#include "button.h"
+
+#define BUTTON_POLL_RATE 100
+
+/* Define button configuration.  */
+static const button_cfg_t sleep_button_cfg =
+{
+    .pio = SLEEP_BUTTON_PIO
+};
+
+
+//Configures the pin used for waking up
+static mcu_sleep_wakeup_cfg_t wkup_config =
+{
+    .pio = SLEEP_BUTTON_PIO, //pin for wakeup
+    .active_high = false // Wakeup on high signal = false
+};
+
+
+// Configures how deep asleep to go
+static mcu_sleep_cfg_t sleep_config =
+{
+    .mode = MCU_SLEEP_MODE_WAIT //this can be changed to how deep asleep it goes?
+};
+
+
+//checks wakeup pin and puts mcu to sleep
+void enter_sleep_mode(void)
+{
+    if (!mcu_sleep_wakeup_set(&wkup_config))
+    {
+        printf("uhoh no wakeup set");
+        return;
+    }
+    mcu_sleep(&sleep_config);
+}
+/* Exit from backup mode occurs if there is an event on the
+    WKUPEN0-15 pins, supply monitor (SM), RTC alarm, or RTT
+    alarm.  The supply monitor monitors the voltage on the
+    VDDIO pin if it is enabled.  The MCU is reset.  */
+
+
+
+int main (void)
+{
+    button_t sleep_button;
+    sleep_button = button_init (&sleep_button_cfg);
+
+    button_poll_count_set (BUTTON_POLL_COUNT (BUTTON_POLL_RATE));
+    pacer_init (BUTTON_POLL_RATE);
+
+    while (1)
+    {
+        pacer_wait ();
+        button_poll (sleep_button);
+        if (button_pushed_p (sleep_button))
+        {
+            enter_sleep_mode();
+        }
+    }
+    return 0;
+}
+
